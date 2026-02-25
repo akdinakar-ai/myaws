89b4414967b6931035914cdeb002b3b941f44fd6
Dinnu <dinnu@Dinnus-MacBook-Pro.local>
2026-02-25 22:05:01 +1100

    ci: trigger terraform-plan after IAM policy update

--- iam-policy.tf
+++ iam-policy.tf
@@ -29,13 +29,32 @@ resource "aws_iam_policy" "tf_ec2_alb_asg_limited" {
           "s3:GetObject",
           "s3:PutObject",
           "s3:ListBucket",
-          "s3:GetBucketLocation"
+          "s3:GetBucketAcl",
+          "s3:GetBucketCORS",
+          "s3:GetBucketVersioning",
+          "s3:GetBucket*",
+          "s3:GetBucketWebsite",
+          "s3:GetBucketVersioning",
+          "s3:GetBucketLogging",
+          "s3:GetBucketEncryption",
+          "s3:GetBucketPolicyStatus",
+          "s3:GetBucketLocation",
+          "s3:GetBucketPolicy"
         ],
         Resource = [
           aws_s3_bucket.backend.arn,
           "${aws_s3_bucket.backend.arn}/*"
         ]
       },
+        {
+          Effect = "Allow",
+          Action = [
+            "s3:GetAccelerateConfiguration"
+          ],
+          Resource = [
+            aws_s3_bucket.backend.arn
+          ]
+        },
       {
         Effect = "Allow",
         Action = [
@@ -43,7 +62,12 @@ resource "aws_iam_policy" "tf_ec2_alb_asg_limited" {
           "dynamodb:GetItem",
           "dynamodb:UpdateItem",
           "dynamodb:Scan",
-          "dynamodb:Query"
+          "dynamodb:Query",
+          "dynamodb:DescribeTable",
+          "dynamodb:DescribeTimeToLive",
+          "dynamodb:DescribeContinuousBackups",
+          "dynamodb:ListTagsOfResource",
+          "dynamodb:DeleteItem"
         ],
         Resource = aws_dynamodb_table.backend.arn
       }
@@ -51,6 +75,37 @@ resource "aws_iam_policy" "tf_ec2_alb_asg_limited" {
   })
 }
 
+
+resource "aws_iam_policy" "tf_oidc_read" {
+  name   = "tf-oidc-read"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "iam:GetOpenIDConnectProvider",
+          "iam:GetRole",
+          "iam:ListAttachedRolePolicies",
+          "iam:GetPolicy",
+          "iam:GetPolicyVersion",
+          "iam:ListRolePolicies",
+          "iam:GetRolePolicy"
+        ],
+        Resource = [
+          "arn:aws:iam::901635709382:oidc-provider/token.actions.githubusercontent.com",
+          "arn:aws:iam::901635709382:role/tf-github-actions-role",
+          "arn:aws:iam::901635709382:policy/tf-oidc-read"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_oidc_read" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.tf_oidc_read.arn
+}
 resource "aws_iam_role_policy_attachment" "attach_tf_policy" {
   role       = aws_iam_role.github_actions.name
   policy_arn = aws_iam_policy.tf_ec2_alb_asg_limited.arn
